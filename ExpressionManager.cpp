55fd9ed368d9b9a51b08d4f118f4b15a67459da5
Matthew <matthew-p@live.com>
2018-10-01 15:50:36 -0600

    Added expressionmanager as well as updated files

---
+++ ExpressionManager.cpp
@@ -0,0 +1,27 @@
+#pragma once
+#include "ExpressionManager.h"
+#include <iostream>
+#include <string>
+
+ExpressionManager(){
+
+}
+~ExpressionManager(){
+
+}
+
+bool ExpressionManager::isBalanced(string expression){
+
+}
+
+string ExpressionManager::postfixToInfix(string postfixExpression){
+
+}
+
+string ExpressionManager::postfixEvaluate(string postfixExpression){
+
+}
+
+string ExpressionManager::infixToPostfix(string infixExpression){
+
+}
