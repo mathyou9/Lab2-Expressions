c7064197ad53112c383bb61a905869936d6ef1c3
matthew <matthew-p@live.com>
2018-10-01 22:27:28 -0600

    updated

--- ExpressionManager.cpp
+++ ExpressionManager.cpp
@@ -7,16 +7,40 @@ using namespace std;
 
 bool ExpressionManager::isBalanced(string expression){
   char charSet;
+  while(!stackOfChars.empty()){
+    stackOfChars.pop();
+  }
   for (int i=0; i < expression.length(); i++){
     if(expression.at(i) == '(' || expression.at(i) == '[' || expression.at(i) == '{'){
       stackOfChars.push(expression.at(i));
       cout << stackOfChars.top() << " ";
     }
-    else if(expression.length() == 0){
+    else if(expression.length() <= 1){
       return false;
     }
-    cout << endl;
+    else if(expression.at(i) == ')' || expression.at(i) == ']' || expression.at(i) == '}'){
+      cout << expression.at(i) << " ";
+      if(stackOfChars.top() == '(' && expression.at(i) == ')'){
+        stackOfChars.pop();
+        if(stackOfChars.empty()){
+          return true;
+        }
+      } else if(stackOfChars.top() == '[' && expression.at(i) == ']'){
+        stackOfChars.pop();
+        if(stackOfChars.empty()){
+          return true;
+        }
+      } else if(stackOfChars.top() == '{' && expression.at(i) == '}'){
+        stackOfChars.pop();
+        if(stackOfChars.empty()){
+          return true;
+        }
+      } else{
+        return false;
+      }
+    }
   }
+  cout << endl;
 }
 
 string ExpressionManager::postfixToInfix(string postfixExpression){
